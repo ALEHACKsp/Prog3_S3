27ea3aa1466b4effdc9a6605648580aebfa33388
Erarnitox <Erarnitox@gmail.com>
2019-12-19 12:35:10 +0100

    most of matrix header

--- Prog3_S3/Matrix.h
+++ Prog3_S3/Matrix.h
@@ -9,6 +9,7 @@
 #define MATRIX_H_
 
 #include <vector>
+#include <iostream>
 
 class Matrix {
 private:
@@ -17,6 +18,27 @@ private:
 public:
 	Matrix(unsigned int sizeX = 4, unsigned int sizeY = 4, double init = 0.0);
 	virtual ~Matrix();
+
+	Matrix& operator-();
+	Matrix& operator+=(const Matrix& b);
+	Matrix& operator-=(const Matrix& b);
+	Matrix& operator+=(double b);
+	Matrix& operator-=(double b);
+
+	std::vector<double> operator[](int index);
+
+	friend Matrix& operator+(const Matrix& a, const Matrix& b);
+	friend Matrix& operator-(const Matrix& a, const Matrix& b);
+	friend Matrix& operator+(const Matrix& a, double b);
+	friend Matrix& operator-(const Matrix& a, double b);
+	friend Matrix& operator+(double a, const Matrix& b);
+	friend Matrix& operator-(double a, const Matrix& b);
 };
 
+Matrix& operator+(const Matrix& a, const Matrix& b);
+Matrix& operator-(const Matrix& a, const Matrix& b);
+Matrix& operator+(const Matrix& a, double b);
+Matrix& operator-(const Matrix& a, double b);
+Matrix& operator+(double a, const Matrix& b);
+Matrix& operator-(double a, const Matrix& b);
 #endif /* MATRIX_H_ */
