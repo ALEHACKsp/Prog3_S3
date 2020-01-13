37c4201717b3f1ebbd626e9be8f7844f400924db
Erarnitox <Erarnitox@gmail.com>
2020-01-13 23:12:12 +0100

    all

--- Prog3_S3/Matrix.cpp
+++ Prog3_S3/Matrix.cpp
@@ -8,10 +8,127 @@
 #include "Matrix.h"
 
 Matrix::Matrix(unsigned int sizeX,unsigned int sizeY, double init) {
-	this->werte = new std::vector<std::vector<double>>();
+	this->werte = std::vector<std::vector<double>>();
+	this->resize(sizeX, sizeY, init);
 }
 
-Matrix::~Matrix() {
-	// TODO Auto-generated destructor stub
+void Matrix::resize(unsigned int sizeX, unsigned int sizeY, double init){
+	if(this->werte.size()){
+		this->werte.resize(sizeX);
+
+		for(auto list : this->werte){
+			list.resize(sizeY, init);
+		}
+	}
 }
 
+Matrix Matrix::operator-(){
+	Matrix neg{this->werte.size(), this->werte[0].size()};
+	for(unsigned i = 0; i < this->werte.size(); ++i){
+		for(unsigned j = 0; j < this->werte[i].size(); ++j){
+			neg[i][j] = -this->werte[i][j];
+		}
+	}
+	return neg;
+}
+
+Matrix& Matrix::operator+=(const Matrix& b){
+	*this = *this+b;
+	return *this;
+}
+
+Matrix& Matrix::operator-=(const Matrix& b){
+	*this = *this+b;
+	return *this;
+}
+
+Matrix& Matrix::operator+=(double b){
+		*this = *this+b;
+		return *this;
+}
+
+Matrix& Matrix::operator-=(double b){
+	*this = *this-b;
+	return *this;
+}
+
+std::vector<double>& Matrix::operator[](int index){
+	return this->werte[index];
+}
+
+std::ostream& operator<<(std::ostream& o, const Matrix& m){
+	for(unsigned i=0; i < m.werte.size(); ++i){
+		for(unsigned j=0; j < m.werte[i].size(); ++j){
+			o << m.werte[i][j] << '\t';
+		}
+		o << '\n';
+	}
+	return o;
+}
+
+std::istream& operator>>(std::istream& is, Matrix& m){
+	for(unsigned i=0; i < m.werte.size(); ++i){
+		for(unsigned j=0; j < m.werte[i].size(); ++j){
+			is >> m[i][j];
+		}
+	}
+	return is;
+}
+
+Matrix operator+(const Matrix& a, const Matrix& b){
+	if(a.werte.size() >= b.werte.size() && a.werte.size() > 0
+			&& a.werte[0].size() >= b.werte[0].size()){
+		Matrix c = a;
+		for(unsigned i = 0; i < b.werte.size(); ++i){
+			for(unsigned j = 0; j < b.werte[i].size(); ++j){
+				c.werte[i][j] += b.werte[i][j];
+			}
+		}
+		return c;
+	}else{
+		return (b + a);
+	}
+}
+
+Matrix operator-(const Matrix& a, const Matrix& b){
+	if(a.werte.size() >= b.werte.size() && a.werte.size() > 0
+				&& a.werte[0].size() >= b.werte[0].size()){
+			Matrix c = a;
+			for(unsigned i = 0; i < b.werte.size(); ++i){
+				for(unsigned j = 0; j < b.werte[i].size(); ++j){
+					c.werte[i][j] -= b.werte[i][j];
+				}
+			}
+			return c;
+		}else{
+			return (b + a);
+		}
+}
+
+Matrix operator+(const Matrix& a, double b){
+	Matrix c = a;
+	for(unsigned i = 0; i < c.werte.size(); ++i){
+		for(unsigned j = 0; j < c.werte[i].size(); ++j){
+			c.werte[i][j] += b;
+		}
+	}
+	return c;
+}
+
+Matrix operator-(const Matrix& a, double b){
+	Matrix c = a;
+	for(unsigned i = 0; i < c.werte.size(); ++i){
+		for(unsigned j = 0; j < c.werte[i].size(); ++j){
+			c.werte[i][j] -= b;
+		}
+	}
+	return c;
+}
+
+Matrix operator+(double a, const Matrix& b){
+	return b+a;
+}
+
+Matrix operator-(double a, const Matrix& b){
+	return b-a;
+}
--- Prog3_S3/Matrix.h
+++ Prog3_S3/Matrix.h
@@ -12,37 +12,38 @@
 #include <iostream>
 
 class Matrix {
+
 private:
 	std::vector<std::vector<double>> werte;
 	void resize(unsigned int sizeX, unsigned int sizeY, double init = 0.0);
 public:
-	Matrix(unsigned int sizeX = 4, unsigned int sizeY = 4, double init = 0.0);
-	virtual ~Matrix();
+	explicit Matrix(unsigned int sizeX = 4, unsigned int sizeY = 4, double init = 0.0);
+	virtual ~Matrix() = default;
 
-	Matrix& operator-();
+	Matrix operator-();
 	Matrix& operator+=(const Matrix& b);
 	Matrix& operator-=(const Matrix& b);
 	Matrix& operator+=(double b);
 	Matrix& operator-=(double b);
 
-	std::vector<double> operator[](int index);
+	std::vector<double>& operator[](int index);
 
-	friend std::ostream operator<<(std::ostream o, const Matrix& m);
-	friend std::istream operator>>(std::istream o, const Matrix& m);
-	friend Matrix& operator+(const Matrix& a, const Matrix& b);
-	friend Matrix& operator-(const Matrix& a, const Matrix& b);
-	friend Matrix& operator+(const Matrix& a, double b);
-	friend Matrix& operator-(const Matrix& a, double b);
-	friend Matrix& operator+(double a, const Matrix& b);
-	friend Matrix& operator-(double a, const Matrix& b);
+	friend std::ostream& operator<<(std::ostream& o, const Matrix& m);
+	friend std::istream& operator>>(std::istream& is, Matrix& m);
+	friend Matrix operator+(const Matrix& a, const Matrix& b);
+	friend Matrix operator-(const Matrix& a, const Matrix& b);
+	friend Matrix operator+(const Matrix& a, double b);
+	friend Matrix operator-(const Matrix& a, double b);
+	friend Matrix operator+(double a, const Matrix& b);
+	friend Matrix operator-(double a, const Matrix& b);
 };
 
-std::ostream operator<<(std::ostream o, const Matrix& m);
-std::istream operator>>(std::istream o, const Matrix& m);
-Matrix& operator+(const Matrix& a, const Matrix& b);
-Matrix& operator-(const Matrix& a, const Matrix& b);
-Matrix& operator+(const Matrix& a, double b);
-Matrix& operator-(const Matrix& a, double b);
-Matrix& operator+(double a, const Matrix& b);
-Matrix& operator-(double a, const Matrix& b);
+std::ostream& operator<<(std::ostream& o, const Matrix& m);
+std::istream& operator>>(std::istream& is, Matrix& m);
+Matrix operator+(const Matrix& a, const Matrix& b);
+Matrix operator-(const Matrix& a, const Matrix& b);
+Matrix operator+(const Matrix& a, double b);
+Matrix operator-(const Matrix& a, double b);
+Matrix operator+(double a, const Matrix& b);
+Matrix operator-(double a, const Matrix& b);
 #endif /* MATRIX_H_ */
